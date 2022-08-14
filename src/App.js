4dbfcd53f08069dd46af735e866c9390ccf8806f
daanvanderputte <daanvanderputte@protonmail.com>
2022-08-14 18:52:09 +0200

    Fetching API and testing artist search

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import axios from "axios"
+
+function App() {
+
+  const [data, setData] = useState({});
+  const [artist, setArtist] = useState('');
+
+  // const url = `https://api.discogs.com/database/search?q=Nirvana&key=${process.env.REACT_APP_API_KEY}&secret=${process.env.REACT_APP_SECRET}`;
+
+  const url = `https://api.discogs.com/database/search?q=${artist}&key=${process.env.REACT_APP_API_KEY}&secret=${process.env.REACT_APP_SECRET}`;
+
+  const searchArtist = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setArtist('')
+    }  
+  }
+
+  return (
+    <div className="App">
+      <div>Record Store</div>
+      <div className="search">
+        <input 
+        value={artist} 
+        onChange={event => setArtist(event.target.value)}
+        onKeyPress={searchArtist}
+        placeholder="Enter Artist"
+        type="text"/>
+      </div>
+      <div className="result">
+            <p>{data.results && data.results[0].title}</p>
+            <img src={data.results && data.results[0].thumb}/>
+          
+          </div>
+    </div>
+  );
+}
+
+export default App;
