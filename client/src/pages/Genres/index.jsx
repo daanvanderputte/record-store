b6ed65646a53167ed3012fdf170ef19ba326f27d
daanvanderputte <daanvanderputte@protonmail.com>
2022-11-10 16:28:24 +0100

    added client server structure

---
+++ client/src/pages/Genres/index.jsx
@@ -0,0 +1,13 @@
+export default function Genres() {
+    return <div>
+        <div className="product-card">
+            <div className="product-image">
+                <img src=""/>
+            </div>
+            <div className="product-title"></div>
+            <div className="product-price"></div>
+        </div>
+
+    
+    Genres</div>
+} 
